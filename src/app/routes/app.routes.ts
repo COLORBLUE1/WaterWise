a282592d6fbbf67c0c67a3018a4063860fef430c
Abel <camilosol123@gmail.com>
2025-04-29 16:00:02 -0500

    ✨ feat:

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { SplashScreenComponent } from '../pages/splash-screen/splash-screen.component';
 
 export const routes: Routes = [
-
+  { path: '/', component: SplashScreenComponent },
+  // { path: 'second-component', component: SecondComponent },
+  // { path: '**', component: PageNotFoundComponent },
 ];
