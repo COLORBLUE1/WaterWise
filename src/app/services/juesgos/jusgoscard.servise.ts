2f3c0fbfa5dd04f3936eefdd809b61ac59297569
Camilo sol <camilosol123@gmail.com>
2025-05-22 13:32:53 -0500

    🐞 fix:

--- src/app/services/juesgos/jusgoscard.servise.ts
+++ src/app/services/juesgos/jusgoscard.servise.ts
@@ -1,12 +1,21 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { inject, Injectable } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
+import { PLATFORM_ID } from '@angular/core';
+
 
 @Injectable({ providedIn: 'root' })
 export class JuegosService {
+  platformId = inject(PLATFORM_ID);
+
   constructor(private http: HttpClient) {}
 
-  getJuegos(): Observable<any[]> {
+ getJuegos() {
+    if (isPlatformServer(this.platformId)) {
+      // Estamos en prerender/SSR, devuelve array vacío o mock
+      return of([]);
+    }
     return this.http.get<any[]>('/api/juegos');
   }
 }
